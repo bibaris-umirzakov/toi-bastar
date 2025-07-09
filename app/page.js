869c41de83fa8adde1004b08978512aeee086b8e
bibaris-umirzakov <bibarisumirzakov0231201@gmail.com>
2025-07-09 12:46:12 +0500

    changed

--- app/page.js
+++ app/page.js
@@ -8,32 +8,7 @@ export default function Home() {
   return (
   <div className="px-4 py-6 max-w-6xl mx-auto">
     <Header />
-      {/* <h1 className="text-3xl font-bold mb-2 text-red-900">Найди свою идеальную Тойхану</h1>
-      <p className="text-gray-600 mb-6">Смотри фото, цены и бронируй прямо сейчас</p>
-
-      <div className="flex flex-col sm:flex-row gap-4 mb-6">
-        <input
-          type="text"
-          placeholder="Поиск по названию или району"
-          className="flex-1 px-4 py-2 border rounded-lg"
-        />
-        <button className="bg-blue-600 text-white px-6 py-2 rounded-lg">Найти Тойхану</button>
-        <button className="bg-green-600 text-white px-6 py-2 rounded-lg">Добавить свою</button>
-      </div>
-
-      <h2 className="text-2xl font-semibold mb-4">🔥 ТОП 3 Тойханы</h2>
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
-        {halls.slice(0, 3).map((hall) => (
-          <HallCard key={hall.id} hall={hall} />
-        ))}
-      </div>
-
-      <h2 className="text-2xl font-semibold mt-10 mb-4">🆕 Новые Тойханы</h2>
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
-        {halls.map((hall) => (
-          <HallCard key={hall.id} hall={hall} />
-        ))}
-      </div> */}
+     
     </div>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-
+import Link from "next/link";
 
 
 export default function Header() {
@@ -6,10 +6,10 @@ export default function Header() {
   <div className="flex items-center gap-6 justify-between px-6 py-4">
     <img src="./Logo.png" className="w-[187px] h-[48px]"/>
     <div className="flex gap-6 text-[16px] font-bold">
-        <a href="#">Главное</a>
-        <a href="#">Контакты</a>
-        <a href="#">Аренда</a>
-        <a href="#">Техподдержка</a>
+        <Link href="/">Главная</Link>
+        <Link href="/pages/contacts">Контакты</Link>
+        <Link href="/pages/rent">Аренда</Link>
+        <Link href="/pages/support">Техподдержка</Link>
     </div>
     <button className="bg-[#FFA500] text-white px-4 py-2 rounded-full hover:bg-gray-100 transition whitespace-nowrap">
         Стать партнёром
