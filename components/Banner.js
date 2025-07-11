7c0feb94c24bdc5eba0b77d048f8c1d1497491ae
oral0005 <oralanykbajuly@gmail.com>
2025-07-11 12:07:02 -0700

    search section added

--- components/Banner.js
+++ components/Banner.js
@@ -1,9 +1,18 @@
-import Image from "next/image"
-export default function Banner(){
-    return(
-        <div className="px-6 pt-15 bg-[url('/banner.jpg')] bg-cover bg-center h-screen w-full">
-            <h1 className="text-[42px] text-white font-bold w-[550px] leading-tight mb-4">Найди идеальный зал и бронируй за пару кликов</h1>
-            <p className="text-white w-[350px] text-[19px] leading-tight">Удобный сервис аренды банкетных залов в твоем городе</p>
+import Image from "next/image";
+import SearchSection from "./SearchSection";
+
+export default function Banner() {
+    return (
+        <div className="px-6 pt-16 bg-[url('/banner.jpg')] bg-cover bg-center h-[800px] w-full flex flex-col items-start justify-start">
+            <div className="ml-20">
+                <h1 className="text-[48px] text-white font-bold w-[600px] leading-tight mb-6">
+                    Найди идеальный зал и бронируй за пару кликов
+                </h1>
+                <p className="text-white w-[400px] text-[22px] leading-tight mb-8">
+                    Удобный сервис аренды банкетных залов в твоем городе. Открой для себя лучшие места для мероприятий!
+                </p>
+                <SearchSection />
+            </div>
         </div>
-    )
+    );
 }
