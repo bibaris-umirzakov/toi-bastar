fbfe46625d7fe8b80f8e77898538b961c0a9d482
marksolo001 <you@example.com>
2025-07-16 16:55:48 +0500

    Сақтадым: contacts/page.js өзгертілді

--- app/pages/contacts/page.js
+++ app/pages/contacts/page.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Header from '@/components/Header'; 
 
 
 
 export default function Contacts() {
   return (
   <div className="">
- Contacts
-    </div>
+      < Header />
+     <div className='p-[55px] flex justify-center items-center'>
+       <div className='bg-[url(/backroundBanket.png)] w-[1170px] h-[587px] rounded-[10px] flex justify-center items-center' >
+       </div>
+     </div>
+ </div> 
   );
 }
