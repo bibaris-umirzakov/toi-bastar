d86cb302e92f2d35cb49af22e3724c0a290dc647
symbat-s <sssymbats0@gmail.com>
2025-07-21 22:02:47 +0500

    rent2

--- app/pages/rent/page.js
+++ app/pages/rent/page.js
@@ -7,16 +7,19 @@ export default function Rent() {
   return (
   <div>
     <Header/>
-    <div className='ml-20'>
+    <div className=' m-8 md:ml-20'>
     <h1 className='font-bold text-3xl mt-10'>Кто мы?</h1>
-    <h1 className="mt-10 text-[20px] w-[800px] tracking-10 font-bold text-[#FFB633]">Toi<span className="text-[#063C5E] font-bold">Bastar </span><span className='text-black font-medium'> -  это стартап, содянный, для того, чтобы упростить поиск и бронирование банкетных залов. Мы помотаем людям экономить время и силы при канчали.</span></h1>
-    <ul className='text-black text-sm'>
-      <li className='flex gap-2 mt-10'><Image src="/icons/check.png" alt="Check" width={22} height={22} />Более 100 залов на одной платформе</li>
-      <li className='flex gap-2 mt-10'><Image src="/icons/check.png" alt="Check" width={22} height={22} />Удобный фильтр по цене и количеству гостей</li>
-      <li className='flex gap-2 mt-10'><Image src="/icons/check.png" alt="Check" width={22} height={22} />Прамое бронирование без переплат</li>
-      <li className='flex gap-2 mt-10'><Image src="/icons/check.png" alt="Check" width={22} height={22} />Поддержка и консультация 24/7</li>
-      <li className='flex gap-2 mt-10 mb-15'><Image src="/icons/check.png" alt="Check" width={22} height={22} />Экономия времени — все залы и контакты собраны в одном месте</li>
+    <h1 className="mt-10 text-sm md:text-[20px] md:w-[800px] tracking-10 font-bold text-[#FFB633] mb-10">Toi<span className="text-[#063C5E] font-bold">Bastar </span><span className='text-black font-medium'> -  это стартап, содянный, для того, чтобы упростить поиск и бронирование банкетных залов. Мы помотаем людям экономить время и силы при канчали.</span></h1>
+    <div className='flex justify-between'>
+    <ul className='text-black text-sm space-y-10'>
+      <li className='flex items-start gap-2'><Image src="/icons/check.png" alt="Check" width={22} height={22} className="w-[22px] h-[22px] flex-shrink-0"/>Более 100 залов на одной платформе</li>
+      <li className='flex items-start gap-2'><Image src="/icons/check.png" alt="Check" width={22} height={22} className="w-[22px] h-[22px] flex-shrink-0"/>Удобный фильтр по цене и количеству гостей</li>
+      <li className='flex items-start gap-2'><Image src="/icons/check.png" alt="Check" width={22} height={22} className="w-[22px] h-[22px] flex-shrink-0"/>Прамое бронирование без переплат</li>
+      <li className='flex items-start gap-2'><Image src="/icons/check.png" alt="Check" width={22} height={22} className="w-[22px] h-[22px] flex-shrink-0"/>Поддержка и консультация 24/7</li>
+      <li className='flex items-start gap-2  mb-15'><Image src="/icons/check.png" alt="Check" width={22} height={22} className="w-[22px] h-[22px] flex-shrink-0"/>Экономия времени — все залы и контакты собраны в одном месте </li>
     </ul>
+    <Image src="/rent.png" alt="" width={250} height={400} className='md:absolute md:top-45 md:left-200 md:block object-contain hidden'/>
+    </div>
     <Link 
     href={'/pages/contacts'}
     className='rounded-lg border-2 border-[#FFA500] text-[#FFA500] px-4 py-2' 
