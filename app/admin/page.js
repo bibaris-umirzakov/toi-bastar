f1a59ae5be1378cfb21008b641eed28773b40343
bibaris-umirzakov <bibarisumirzakov0231201@gmail.com>
2025-07-28 16:56:01 +0500

    login

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,10 +1,21 @@
+'use client'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
-export default function Admin() {
+export default function AdminPage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const isAdmin = localStorage.getItem('isAdmin')
+    if (isAdmin !== 'true') {
+      router.push('/admin/login')
+    }
+  }, [])
+
   return (
-
-  <div className="">
- Admin Page
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Админка: Добавить Тойхану</h1>
+      {/* Форма добавления */}
     </div>
-
-  );
+  )
 }
